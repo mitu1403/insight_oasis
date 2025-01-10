84a24ce8d406add78a1a51e359aa4db1f08d02d2
mitu1403 <mmitu163069@bscse.uiu.ac.bd>
2025-01-10 19:41:14 +0600

    displaying data from local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import Header from "./components/Header";
 import Blogs from "./components/Blogs/Blogs";
 import Bookmarks from "./components/Bookmarks/Bookmarks";
 // import { getStoredBookmark, addToLocalStorage } from "./Utils/localStorage";
-import { getStoredData, addToLocalStorage } from "./Utils/localStorage";
+import { addToLocalStorage } from "./Utils/localStorage";
 
 function App() {
   const [bookmarkItems, setBookmarkItems] = useState([]);
@@ -12,21 +12,17 @@ function App() {
   const handleBookmarks = (blog) => {
     if (!bookmarkItems.includes(blog)) {
       setBookmarkItems([...bookmarkItems, blog]);
-      addToLocalStorage(blog.id, 'bookmark')
+      addToLocalStorage(blog.id, "bookmark");
     }
   };
   const handleSpentTime = (time, blog) => {
     setSpentTime((prev) => prev + time);
-    addToLocalStorage(time, 'spentTime')
+    addToLocalStorage(time, "spentTime");
     const remainingBookmarkItem = bookmarkItems.filter(
       (item) => item.id != blog.id
     );
     setBookmarkItems(remainingBookmarkItem);
   };
-  // useEffect(() =>{
-  //   const data = getStoredBookmark();
-  //   console.log(data)
-  // },[])
   return (
     <div className="max-w-[1280px] mx-auto px-5 parent-scrollbar">
       <Header />
@@ -34,6 +30,8 @@ function App() {
         <Blogs
           handleBookmarks={handleBookmarks}
           handleSpentTime={handleSpentTime}
+          setBookmarkItems={setBookmarkItems}
+          setSpentTime={setSpentTime}
         />
         <Bookmarks bookmarkItems={bookmarkItems} spentTime={spentTime} />
       </div>
--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -1,7 +1,13 @@
 import { useEffect, useState } from "react";
 import Blog from "./Blog";
+import { getStoredData } from "../../Utils/localStorage";
 
-function Blogs({ handleBookmarks, handleSpentTime }) {
+function Blogs({
+  handleBookmarks,
+  handleSpentTime,
+  setBookmarkItems,
+  setSpentTime,
+}) {
   const [blogs, setBlogs] = useState([]);
 
   useEffect(() => {
@@ -9,6 +15,23 @@ function Blogs({ handleBookmarks, handleSpentTime }) {
       .then((response) => response.json())
       .then((data) => setBlogs(data));
   }, []);
+
+  useEffect(() => {
+    if (blogs.length > 0) {
+      const bookmarkData = getStoredData("bookmark");
+      const spentTimeData = getStoredData("spentTime");
+      console.log(spentTimeData);
+      const bookmarkItems = [];
+      for (const item of bookmarkData) {
+        const theItem = blogs.find((i) => i.id === item);
+        if (theItem) {
+          bookmarkItems.push(theItem);
+        }
+      }
+      setBookmarkItems(bookmarkItems);
+      setSpentTime(spentTimeData);
+    }
+  }, [blogs]);
   return (
     <div className="md:w-2/3 pe-5 h-[100vh] overflow-y-scroll blogs">
       {blogs.map((item) => (
